c7c039bb2210a17736891314c2473f0b0ea95700
Cristian Quito <cristianandresquito@gmail.com>
2023-11-24 16:38:37 -0500

    fix: cors

--- src/main/java/com/upc/tplanner/TPlanner/security/SecurityConfig.java
+++ src/main/java/com/upc/tplanner/TPlanner/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -38,6 +39,8 @@ public class SecurityConfig {
         JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtUtil);
         jwtAuthenticationFilter.setAuthenticationManager(authenticationManager);
         jwtAuthenticationFilter.setFilterProcessesUrl("/login");
+
+        httpSecurity.cors(Customizer.withDefaults());
 
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
