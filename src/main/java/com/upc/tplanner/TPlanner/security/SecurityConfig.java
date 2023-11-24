f1383111417895210039548b8a8bd19ba327154f
Cristian Quito <cristianandresquito@gmail.com>
2023-11-24 16:57:09 -0500

    fix: cors

--- src/main/java/com/upc/tplanner/TPlanner/security/SecurityConfig.java
+++ src/main/java/com/upc/tplanner/TPlanner/security/SecurityConfig.java
@@ -19,6 +19,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableMethodSecurity
@@ -39,8 +44,6 @@ public class SecurityConfig {
         JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtUtil);
         jwtAuthenticationFilter.setAuthenticationManager(authenticationManager);
         jwtAuthenticationFilter.setFilterProcessesUrl("/login");
-
-        httpSecurity.cors(Customizer.withDefaults());
 
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
@@ -79,6 +82,17 @@ public class SecurityConfig {
                 .passwordEncoder(passwordEncoder);
 
         return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class).build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
